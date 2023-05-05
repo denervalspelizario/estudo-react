a6195e86b96dae570e94abd9ee92d195057d41a7
denervalspelizario <denervalspelizario@gmail.com>
2023-05-05 13:26:58 -0300

    estudo react uso de link ativeo e pagina de erro

--- React-router/src/pages/Product.js
+++ React-router/src/pages/Product.js
@@ -9,24 +9,28 @@ import { useFetch } from "../hooks/useFetch"  //  importando o useFetch
 const Product = () => {
 
     //  rota dinamica
-    const { id } = useParams()  // useParams traz o id do produto - obs ta recebendo o id porque la app.js a route é products/id
+    const { id } = useParams()  // useParams traz o id do produto - no home.js recebe no link da rota dinamica esta indicado item.id 
+                                //  - nao esquececer de em app.js deixar a rota dinamica :id
     console.log(id)
 
     // 5 - carregamento dado individual
     const url = "http://localhost:3000/products/" + id;   // url da api para fazer requisição dos dados da data ** o run server tem que estar ativo senao nao funciona atentar a url***
                                                          // concatenada com id ** nao esquecer o '/' no final
 
-    const {data: product, loading, error} = useFetch(url); // chamando o useFectch la do hook com o parametro url da json //  chamando o httpConfig e o loading tb
-                                                          // o items é uma renomeacao mas poderia ser qualquer nome ou seja data vira items   
+    const {data: product, loading, error} = useFetch(url); // chamando o useFectch la do hook com todos os hooks 
+                                                          // o product é uma renomeacao mas poderia ser qualquer nome ou seja data vira product   
 
     console.log(product);                                                          
 
   return (
     <div>
         <p>ID do produto: {id}</p>
+
         {error && <p>Ocorreu um erro...</p>}  {/*debug ja condicionando se caso ouver o erro ja diz qual o erro */}
+        
         {loading && <p>Carregando...</p>}  {/* Carregamento se houver demora */}
-        {product && (  
+        
+        {product && (  // deu certo carregou então carrego todos ja estruturado
           <div>
             <h1>{product.name}</h1>  {/* Carregamento dinamico do name do produto*/}
             <p>R${product.price}</p> {/* Carregamento dinamico do preco do produto*/}
