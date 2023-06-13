e3c1ec5c564c303f0d7b9ab2acd28104456b6f99
denervalspelizario <denervalspelizario@gmail.com>
2023-06-13 11:25:47 -0300

    estudo react revisao de criacao de componets react + typescript + uso de props com interface

--- 01_PrimeiroProjeto/src/App.tsx
+++ 01_PrimeiroProjeto/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/header";
+import Aluno from './components/aluno';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  return(
+    <div>
+      <Header/>
+      <h1>Meu projeto</h1>
+      <Aluno  
+       nome="Ana Caroline"  // adicionando dado ao props que é uma interface tipada chamada AlunoProps
+       idade={19}
+      /> 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Meu primeiro projeto</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Aluno  
+       nome="Fabio Silva"  // adicionando dado ao props que é uma interface tipada chamada AlunoProps
+       idade={22}
+      /> 
+    </div>
   )
 }
 
-export default App
+
---
+++ 01_PrimeiroProjeto/src/components/header.tsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+export default function Header(){
+  return (
+    <header className='header'>
+      <h1 className='title'>Alunos do React + TypeScript</h1>
+      <hr />
+    </header>
+  )
+}
