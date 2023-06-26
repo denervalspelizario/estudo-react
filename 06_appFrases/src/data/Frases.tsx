eaf26f4e77d89510dc1cb7bdfe10e33f9a4dfabd
denervalspelizario <denervalspelizario@gmail.com>
2023-06-26 10:50:00 -0300

    estudo react appfrases adicao de logica para gerar frases aleatoria

---
+++ 06_appFrases/src/data/Frases.tsx
@@ -0,0 +1,33 @@
+
+export const frasesCharlieBrown = [
+  {
+    id: 1,
+    nome: 'Lutar pelo o que é meu',
+    frases : [
+      'A gente passa a entender melhor a vida quando encontra o verdadeiro amor',
+      'De qualquer jeito, seu sorriso vai ser meu raio de Sol',
+      'Lutar pelo o que é meu',
+    ]
+  },
+  {
+    id: 2,
+    nome: 'Vicios e Virtudes',
+    frases : [
+      'Deixa eu te mostrar que a vida pode ser melhor, mesmo sendo tão louca',
+      'Logo eu, que sempre achei legal ser tão errado',
+      'Eu nunca tive muito a ver com ela, o livro que ela ama eu não li. Eu nunca tive muito a ver com ela, o filme que ela adora eu não vi',
+      'Se eu não puder fazer você a pessoa mais feliz, eu chego o mais perto disso possível!',
+      'O tempo às vezes é alheio à nossa vontade, mas só o que é bom dura tempo o bastante pra se tornar inesquecível'
+    ]
+  },
+  {
+    id: 3,
+    nome: 'Lugar Ao Sol',
+    frases : [
+      'Livre pra poder sorrir, sim, livre pra poder buscar o meu lugar ao Sol',
+      'Ainda vejo o mundo com os olhos de criança',
+      'Nossas vidas, nossos sonhos têm o mesmo valor',
+    ]
+  },
+ 
+]
